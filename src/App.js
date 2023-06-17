334eb180869d4c3bc016135e6eab00556dd4d1e6
Rhayane Cassemiro <rhayane.cassemiro@sapientag2.com.br>
2023-06-16 21:28:13 -0300

    [WIP]ajustes.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState("");
   return (
     <div className='App'>
       <h1>React Todo App</h1>
 
       <div>
-        <input type='text' name='todo' placeholder='Criando um novo todo'/>
+        <input 
+          type='text' 
+          name='todo'
+          value={todo} 
+          placeholder='Criando um novo todo'
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+          />
         <button className='add-button'>
           Add
         </button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
