d7640794492e159684b5c4f7bdb6bddfbecdb014
Rhayane Cassemiro <rhayane.cassemiro@sapientag2.com.br>
2023-06-16 21:19:48 -0300

    [WIP]css e button.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> React Todo App </h1>
-      </header>
+    <div className='App'>
+      <h1>React Todo App</h1>
+
+      <div>
+        <input type='text' name='todo' placeholder='Criando um novo todo'/>
+        <button className='add-button'>
+          Add
+        </button>
+      </div>
     </div>
   );
 }
