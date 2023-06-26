ba47cc1b26309f3611df4a8e2684e5edb0849d5e
Rhayane Cassemiro <rhayane.cassemiro@sapientag2.com.br>
2023-06-26 14:55:29 -0300

    [WIP]add todos.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className='App'>
       <h1>React Todo App</h1>
 
-      <div>
+      <div className='input-wrapper'>
         <input 
           type='text' 
           name='todo'
           value={todo} 
-          placeholder='Criando um novo todo'
+          placeholder='Create a new todo'
           onChange={(e) => {
             setTodo(e.target.value);
           }}
           />
-        <button className='add-button'>
+        <button className='add-button' onClick={addTodo}>
           Add
         </button>
       </div>
+      
+      {todos?.length > 0 ? (
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <div className="todo">
+              <li key={index}> {todo} </li>
+
+              <button className="delete-button">Delete</button>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">
+          <p>No task found</p>
+        </div>
+      )}
     </div>
   );
 };
